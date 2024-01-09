862aff984a6f01f536bf9353aa56b96498471446
Ferry Jérémie <ferryjeremie@free.fr>
2024-01-09 15:50:55 +0100

    php : cwe 89

--- php/cwe/cwe_89/cli_sample.php
+++ php/cwe/cwe_89/cli_sample.php
@@ -3,7 +3,10 @@
 $user_name = $argv[1];
 
 function unsafe($user_name) {
-    $stmt = $pdo->prepare("SELECT * FROM utilisateurs WHERE user_name = '" + $user_name + "'");
+    $pdo = new PDO('sqlite:'.dirname(__FILE__).'/primtuxmenu.db');
+    $select = "SELECT * FROM utilisateurs WHERE user_name = '" . $user_name . "'";
+    echo $select;
+    $stmt = $pdo->prepare($select);
     $stmt->execute();
 }
 
